024cf9f6d860d82ca103ef95e190a3910858feb2
uswa00111 <uswa00111@gmail.com>
2022-07-02 19:18:51 +0500

    new files entries

--- main.js
+++ main.js
@@ -18,5 +18,7 @@ class Block { //WE'LL BEGIN BY MAKING A CLASS OF BLOCK:
     //SUMMARY: means, it'll calculate the hash func, for that we'll need the properties (tracked/cloned properties, we made for reuse) of the bloc, and then it'll generate a new Hash (unique ID) for the block.
     //BEFORE CALCULATING, WE'LL NEED HASH FUNC/HASH-NUMBER, THAT WILL BE PROVIDED BY A LIBRARY CALLED crypto-js (npm). WE'LL INSTALL IT IN OUR CODE THROUGH TERMINAL TO GET THOSE HASH FUNCS.
     calculateHash() {
+        //now we'll return here the sha256 hash of our properties:
+        return SHA256(this.index + this.timeStamp + this.previousHash + JSON.stringify(this.data)) //to make it look good like a database, we have used JSON.stringify, it'll make the data property look good like its in an array.
     }
 }
