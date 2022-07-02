c55056bf7ce81f860cbff2bd0a7981ea2f76e3cb
uswa00111 <uswa00111@gmail.com>
2022-07-02 21:05:00 +0500

    giving data to new block into blockchain

--- main.js
+++ main.js
@@ -63,4 +63,5 @@ class blockChain { //we'll here again give it a constructor:
 
 //NOW TO TEST ALL THE CODE WE WROTE ABOVE, WE'LL CREATE AN 'INSTANCE' OF OUR BLOCKCHAIN:
 //DEFINITION: An instance is an object containing data and behavior described by the class.
-let uswaSBlockChain = 
+let uswaSBlockChain = new newBlockChain(); //here we made a variable and named it newBlockChain.
+uswaSBlockChain.addBlock(new block(1, "02/01/2022", "Account User: Uswa R. Balance: 2 Dodge Coins")) //now here we're giving the new block its details/info/data (index, timeStamp, previousHash, & data)
